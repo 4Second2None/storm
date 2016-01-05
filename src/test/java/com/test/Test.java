595f7b80aa76ac3a19e368af9f79a4667fcdf018
bbaiggey <1273802464@qq.com>
2016-01-05 13:09:11 +0800

    测试hashMap

--- src/test/java/com/test/Test.java
+++ src/test/java/com/test/Test.java
@@ -1,17 +1,35 @@
 package com.test;
 
+import java.util.HashMap;
 import java.util.Random;
 
+import org.apache.commons.collections.map.HashedMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Test {
+	HashMap<String , Integer> hashMap = new HashMap<String, Integer>();
 	@org.junit.Test
 	public void TestLogger(){
 	
 		Logger logger = LoggerFactory.getLogger(Test.class);
 		logger.info("ssssssssssssssss");
 	}
+	@org.junit.Test
+	public void TestHash(){
+		
+		for (int i = 0; i < 12; i++) {
+			
+			
+			hashMap.put(i+"", i);
+		}
+		System.out.println(hashMap.size());
+	}
+	
+	
+	
+	
+	
 	@org.junit.Test
 	public void TestRandom(){
 	
@@ -20,7 +38,6 @@ public class Test {
 		for (int i = 0; i < 1000; i++) {
 			logger.info(random.nextInt()+"");
 		}
-		
 		
 	
 	}
