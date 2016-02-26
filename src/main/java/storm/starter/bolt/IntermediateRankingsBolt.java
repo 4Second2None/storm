cc00d87922ffc61512cdd43d4229b844735995c0
bbaiggey <1273802464@qq.com>
2016-02-26 20:41:55 +0800

    storm.starter.bolt.IntermediateRankingsBolt 这个bolt作用就是对于中间结果的排序, 为什么要增加这步, 应为数据量比较大, 如果直接全放到一个节点上排序, 会负载太重 	所以先通过IntermediateRankingsBolt, 过滤掉一些 	这里仍然使用, 对于obj进行fieldsGrouping, 保证对于同一个obj, 不同时间段emit的统计数据会被发送到同一个task
    	IntermediateRankingsBolt继承自AbstractRankerBolt(参考下面)   
    	并实现了updateRankingsWithTuple,

--- src/main/java/storm/starter/bolt/IntermediateRankingsBolt.java
+++ src/main/java/storm/starter/bolt/IntermediateRankingsBolt.java
@@ -10,6 +10,13 @@ import storm.starter.tools.RankableObjectWithFields;
  * <p/>
  * It assumes the input tuples to adhere to the following format: (object, object_count, additionalField1,
  * additionalField2, ..., additionalFieldN).
+ * 
+ * storm.starter.bolt.IntermediateRankingsBolt 这个bolt作用就是对于中间结果的排序, 为什么要增加这步, 应为数据量比较大, 如果直接全放到一个节点上排序, 会负载太重   
+	所以先通过IntermediateRankingsBolt, 过滤掉一些   
+	这里仍然使用, 对于obj进行fieldsGrouping, 保证对于同一个obj, 不同时间段emit的统计数据会被发送到同一个task
+	
+	IntermediateRankingsBolt继承自AbstractRankerBolt(参考下面)   
+	并实现了updateRankingsWithTuple,
  */
 public final class IntermediateRankingsBolt extends AbstractRankerBolt {
 
