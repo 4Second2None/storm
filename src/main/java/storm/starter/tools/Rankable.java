25f67bac41ebc6693a84ba6f6137246282234f33
bbaiggey <1273802464@qq.com>
2016-02-26 21:01:27 +0800

    Rankable除了继承Comparable接口, 还增加getObject()和getCount()接口

--- src/main/java/storm/starter/tools/Rankable.java
+++ src/main/java/storm/starter/tools/Rankable.java
@@ -1,5 +1,11 @@
 package storm.starter.tools;
-
+/**
+ * 
+ * Description: Rankable除了继承Comparable接口, 还增加getObject()和getCount()接口
+ * Copyright:   ©2015 Vbill Payment Co. Ltd. All rights reserved.
+ * Created on:  2016年2月26日 下午8:45:26 
+ * @author bbaiggey
+ */
 public interface Rankable extends Comparable<Rankable> {
 
   Object getObject();
